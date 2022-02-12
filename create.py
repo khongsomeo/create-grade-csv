6c197e91ad066888db5644aa3decf7a54ccaf669
hwangswan <mrthedzam@gmail.com>
2022-02-12 13:00:56 +0700

    Now it's can run pretty well

--- create.py
+++ create.py
@@ -1,6 +1,9 @@
 import datetime, sys, getopt
 import pandas as pd
 
+def clean_course_code(course_string):
+    return course_string.split('-')[0].strip()
+
 def main(argv):
     input_file, output_file = '', ''
     
@@ -20,11 +23,17 @@ def main(argv):
         print('Missing arguments, please try again')
         sys.exit(2)
 
+    # Preprocessing data with Pandas
     df = pd.read_csv(input_file, sep = '\t', header = None)
     df = df.drop(df.columns[[0, 3, 4, 6]], axis = 1)
+    df.columns = ['course_id', 'course_credits', 'course_grade']   
     df = df.dropna()
-    df[1, :] = df[1, :].split('-')[0].strip()
-    print(df)
+
+    df['course_id']= df['course_id'].apply(clean_course_code)
+    df['course_credits'] = df['course_credits'].astype(int)
+
+    # Saving file as csv
+    df.to_csv(output_file, header = False, index = False)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
